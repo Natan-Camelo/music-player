ce4b984077225bf83ff529c28769883773f7142b
Natan Camelo de Sousa <127790024+Natan-Camelo@users.noreply.github.com>
2024-05-21 20:23:19 -0300

    Update data.js

--- src/js/data.js
+++ src/js/data.js
@@ -1,13 +1,13 @@
-window.data = [
-    {
-        title: "Paradox",
-        artist: "Survive Said The Prophet",
-        cover: "survive-said-the-prophet-paradox.jpg",
-        file: "paradox.mp3"
+// window.data = [
+//     {
+//         title: "Paradox",
+//         artist: "Survive Said The Prophet",
+//         cover: "survive-said-the-prophet-paradox.jpg",
+//         file: "paradox.mp3"
         
         // title: "Roar",
         // artist: "Ulma Sound Junction",
         // cover: "ulma-sound-junction-roar.jpg",
         // file: "ROAR.mp3"
-    }
-]
+//     }
+// ]
